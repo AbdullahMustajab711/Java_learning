ce71cb9cea797e4a555ff8d2731da72b4bf69c26
Abdullah Mustajab <you@example.com>
2026-03-06 01:41:12 +0500

    first commit

---
+++ input.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class input {
+    public static void main(String[] args) {
+
+        try (Scanner sc = new Scanner(System.in)) {
+
+            System.out.print("Enter your number in inches: ");
+            double inches = sc.nextDouble();
+
+            double meter = inches * 0.0254;
+
+            System.out.println(inches + " Inches is equals to: " + meter + " meter");
+
+        }
+    }
+}
---
+++ output.java
@@ -0,0 +1,7 @@
+public class output{
+    public static void main(String[] args) {
+        System.out.println("Hello World"); // println is to add a new line after execution
+        System.out.print("Hello World");  // print is to stay on the same line after execution
+        System.out.println("Hello World\n"); // "\n" is to add a new line after execution
+    }
+}
